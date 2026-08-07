eb11c38413cdfc6dbaab6e45c2b922f351af415c
Muskan Tarafder <160621524+Muskan-Tarafder@users.noreply.github.com>
2026-08-07 20:05:23 +0530

    Time: 3 ms (9.56%) | Memory: 9.4 MB (31.16%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -14,8 +14,31 @@ public:
         int bot=solve(m,n,i+1,j,dp);
         return dp[i][j]=right+bot;
     }
+    int solveTab(int m,int n){
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1)
+                    continue;
+
+                int right = 0;
+                int down = 0;
+
+                if(i+1 < m)
+                    down = dp[i+1][j];
+
+                if(j+1 < n)
+                    right = dp[i][j+1];
+
+                dp[i][j] = right + down;
+            }
+        }
+        return dp[0][0];
+    }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int> (n,-1));
-        return solve(m,n,0,0,dp);
+        // return solve(m,n,0,0,dp);
+        return solveTab(m,n);
     }
 };
