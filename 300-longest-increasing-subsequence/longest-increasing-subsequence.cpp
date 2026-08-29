5644a65e05d5f7dc9a524e3d9a0649c5f791731a
Muskan Tarafder <160621524+Muskan-Tarafder@users.noreply.github.com>
2026-08-29 23:15:20 +0530

    Time: 585 ms (10.61%) | Memory: 295.2 MB (32.39%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int i, int prev,vector<vector<int>> &dp){
+        if(i>=nums.size()){
+            return 0;
+        }
+        if(dp[i][prev+1]!=-1){
+            return dp[i][prev+1];
+        }
+        int notTake = solve(nums, i + 1, prev,dp);
+
+        int take = 0;
+        if (prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + solve(nums, i + 1, i,dp);
+        }
+        // cout<<"take: "<<take<<" nottake:"<<notTake<<endl;
+        return dp[i][prev+1]=max(take, notTake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(),vector<int> (nums.size(),-1));
+        return solve(nums,0,-1,dp);
+    }
+};
