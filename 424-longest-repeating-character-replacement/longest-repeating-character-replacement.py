767501efcabb179efc5a239ed85d0548c43a3dea
Muskan Tarafder <160621524+Muskan-Tarafder@users.noreply.github.com>
2026-09-18 15:03:20 +0530

    Time: 84 ms (54.71%) | Memory: 19.7 MB (52.80%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,12 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l=r=ml=mf=0
-        hm=[0 for i in range(26)]
+        l,r=0,0
+        mf,ml=0,0
+        hm={}
         while(r<len(s)):
-            hm[ord(s[r]) - ord('A')] += 1
-            mf=max(mf,hm[ord(s[r]) - ord('A')])
-            if (r-l+1)-mf>k:
-                hm[ord(s[l]) - ord('A')]-=1
+            hm[s[r]]=hm.get(s[r],0)+1
+            mf=max(mf,hm[s[r]])
+            if(r-l+1)-mf>k:
+                hm[s[l]]-=1
                 l+=1
             ml=max(ml,r-l+1)
             r+=1
